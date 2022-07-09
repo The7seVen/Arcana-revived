6565e7b570cced9979aa533efecbeb1a00a01e04
The7seVen <The_seVen@interia.pl>
2022-07-09 17:21:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Arcana
+for Minecraft 1.19.
+
+Arcana is a magic-themed Minecraft mod, with roots in Thaumcraft.
+
+## License?
+Yea, would be nice to have some...
---
+++ src/main/java/com/arcanarevived/arcana/Arcana.java
@@ -0,0 +1,66 @@
+package com.arcanarevived.arcana;
+
+import com.mojang.logging.LogUtils;
+import com.tterrag.registrate.Registrate;
+import com.tterrag.registrate.util.nullness.NonNullSupplier;
+import net.minecraft.client.Minecraft;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.slf4j.Logger;
+
+@Mod(Arcana.MOD_ID)
+public class Arcana {
+    public static final String MOD_ID = "arcana";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public static final NonNullSupplier<Registrate> REGISTRATE = NonNullSupplier.lazy(() -> Registrate.create(MOD_ID));
+
+    public Arcana() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+
+    public static ResourceLocation ArcLoc(String path) { return new ResourceLocation(MOD_ID, path); }
+
+    public static Registrate getRegistrate() {
+        return REGISTRATE.get();
+    }
+}
